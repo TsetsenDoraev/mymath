54c3c71b5a3bbfd3cbdf972f2d8c54ef6af3f688
Tsetsen_Doraev <doraevtsetsen@gmail.com>
2024-05-30 05:39:21 +0300

    First mymath commit

---
+++ mymath.go
@@ -0,0 +1,19 @@
+package mymath
+
+import "math"
+
+func Sqrt(x float64) int {
+	return math.Sqrt(x)
+}
+
+func Pow(x, y float64) float64 {
+	return math.Pow(x, y)
+}
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
