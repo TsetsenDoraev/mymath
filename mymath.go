17db99d2a86c64dbb729429ab2006fe5fefef3a8
Tsetsen_Doraev <doraevtsetsen@gmail.com>
2024-06-08 02:17:46 +0300

    commit my formulas

--- mymath.go
+++ mymath.go
@@ -2,18 +2,25 @@ package mymath
 
 import "math"
 
-func Sqrt(x float64) int {
-    	return int(math.Sqrt(x))
+func Sqrt(x float64) float64 {
+    z := 1.0
+    for i := 0; i < 10; i++ {
+        z -= (z*z - x) / (2*z)
+    }
+    return z
 }
 
 func Pow(x, y float64) float64 {
-	return math.Pow(x, y)
-}
-
-func Abs(x float64) float64 {
-	return math.Abs(x)
+    result := 1.0
+    for i := 0; i < int(y); i++ {
+        result *= x
+    }
+    return result
 }
 
 func Max(x, y float64) float64 {
-	return math.Max(x, y)
+    if x > y {
+        return x
+    }
+    return y
 }
