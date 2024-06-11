4c1697977d712d5099c5ab74ed42053009358d5e
Tsetsen_Doraev <doraevtsetsen@gmail.com>
2024-06-11 09:43:23 +0300

    321

--- mymath.go
+++ mymath.go
@@ -5,3 +5,14 @@ import "math"
 func Sqrt(x float64) float64 {
     return math.Sqrt(x) 
 }
+
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+}
